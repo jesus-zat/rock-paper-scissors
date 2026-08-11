517d0aaface8338e467319cce988a125fb16deda
jesus-zat <jesusitojzt@gmail.com>
2026-08-11 17:41:32 -0600

    add getHumanChoice function for user input

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ function getComputerChoice() {
     // I use Math.floor because it rounds a number down to the nearest integer less than or equal to the original number
     
     if(randomNumber === 0) {
-        return randomNumber + "Rock";
+        return "Rock";
     } else if(randomNumber === 1){
-        return randomNumber + "Paper";
+        return "Paper";
     } else if(randomNumber === 2) {
-        return randomNumber + "Scissors";
+        return "Scissors";
     } 
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    return prompt("Write Rock, Paper or Scissors");
+}
+
+console.log(getHumanChoice());
